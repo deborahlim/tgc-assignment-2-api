58fce7ae927e2b6399a90450b1292cdc894eb064
deborahlim <deborahlimhy@gmail.com>
2021-08-31 16:03:32 +0000

    add profile route

--- index.js
+++ index.js
@@ -26,20 +26,66 @@ app.get("/special-connections", (req, res, next) => {
   console.log("Root");
 });
 
-app.post("/special-connections/signup", async (req, res, next) => {
+app.get("/special-connections/users", (req, res, next) => {
+  res.status(200).send();
+});
 
+app.post("/special-connections/signup", async (req, res, next) => {
   try {
     let name = req.body.name;
     let email = req.body.email;
-    let gender = req.body.gender;
+    let password = req.body.password;
+    let confirmPassword = req.body.password;
     let datetime = new Date(req.body.datetime) || new Date();
     let db = MongoUtil.getDB();
     // tell mongo to insert the document
     let result = await db.collection("users").insertOne({
       name: name,
       email: email,
+      password: password,
+      confirmPassword: confirmPassword,
+    });
+    res.status(200);
+    res.send(result);
+  } catch (e) {
+    res.status(500);
+    res.send({
+      error: "Internal server error. Please contact administrator",
+    });
+    console.log(e);
+  }
+});
+
+app.post("/special-connections/profile", async (req, res, next) => {
+  try {
+    let dob = req.body.dob;
+    let gender = req.body.gender;
+    let country = req.body.country;
+    let disability = req.body.disability;
+    let interestedIn = req.body.interestedIn;
+    let genderPreference = req.body.genderPreference;
+    let minAge = req.body.minAge;
+    let maxAge = req.body.maxAge;
+    let countryPreference = req.body.countryPreference;
+    let disabilityPreference = req.body.disabilityPreference;
+    let aboutMe = req.body.aboutMe;
+    let interests = req.body.interests;
+
+    let db = MongoUtil.getDB();
+    // tell mongo to insert the document
+    let result = await db.collection("users").insertOne({
+      dob: dob,
       gender: gender,
-      datetime: datetime,
+      country: country,
+      disability: disability,
+      interestedIn: interestedIn,
+      genderPreference: genderPreference,
+      minAge: minAge,
+      maxAge: maxAge,
+      countryPreference: countryPreference,
+      disabilityPreference: disabilityPreference,
+      aboutMe: aboutMe,
+      interests: interests,
     });
     res.status(200);
     res.send(result);
