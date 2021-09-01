b9c8f26522809f5fcd06241e7ea91c68dd7c4508
deborahlim <deborahlimhy@gmail.com>
2021-09-02 00:14:45 +0800

    add user profile matches route

--- index.js
+++ index.js
@@ -98,6 +98,28 @@ app.post("/special-connections/profile", async (req, res, next) => {
   }
 });
 
+app.get("/special-connections/user/:id", async (req, res, next) => {
+  let user_id = req.params.id;
+  let db = MongoUtil.getDB();
+  let criteria = {};
+  console.log(user_id);
+  let result = await db
+    .collection("users")
+    .find({
+      _id: ObjectId(user_id),
+    })
+    .toArray();
+  console.log(result);
+  let matches = await db
+    .collection("users")
+    .find({
+      country: result[0].country,
+    })
+    .toArray();
+
+  res.status(200);
+  res.send(matches);
+});
 // START SERVER
 app.listen(3000, () => {
   console.log("Server has started");
