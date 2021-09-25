8a8fc6030cbffc444f9e605f26f38921ee58f8d7
deborahlim <deborahlimhy@gmail.com>
2021-09-25 05:02:39 +0000

    make messages array length maxmimum 20

--- controllers/chatsController.js
+++ controllers/chatsController.js
@@ -42,14 +42,23 @@ exports.createChats = async function (req, res) {
 
 exports.updateChats = async function (req, res) {
   try {
-    let { room, messages } = req.body;
+    let { room, message } = req.body;
     let db = MongoUtil.getDB();
-    console.log("updated", room);
-    let result = await db
-      .collection("chats")
-      .updateOne({ room: room }, { $push: { messages } });
 
-    res.status(200).send(result);
+    let result = await db.collection("chats").findOne({
+      room: room,
+    });
+
+    if (result.messages.length >= 20) {
+      await db
+        .collection("chats")
+        .updateOne({ room: room }, { $pop: { messages: -1 } });
+    }
+    let response = await db
+      .collection("chats")
+      .updateOne({ room: room }, { $push: { messages: message } });
+
+    res.status(200).send(response);
   } catch (e) {
     res.status(500);
     res.send({
